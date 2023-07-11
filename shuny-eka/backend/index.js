ff59baadaa43eee97348d3a8b15c79e223ea3f8a
jitendra4545 <jitendra45.js45@gmail.com>
2023-07-11 22:26:14 +0530

    backend completed

--- shuny-eka/backend/index.js
+++ shuny-eka/backend/index.js
@@ -8,6 +8,7 @@ const { connection } = require('./config/db')
 require('dotenv').config()
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
 app.get("/users", async (req, res) => {
@@ -15,7 +16,7 @@ app.get("/users", async (req, res) => {
 let user=await UserModel.find()
 res.send(user)
      } catch (err) {
-        res.send(err)
+        res.send({"msg":"somthing went wrong! cannot Get User Data","error":err.message})
 
       }
 
@@ -27,9 +28,9 @@ app.get("/users/:id", async (req, res) => {
     console.log(id)
     try { 
        let one= await UserModel.find({"_id":id})
-res.send(one)
+       res.send(one)
     } catch (err) { 
-        res.send(err)
+        res.send({"msg":"somthing went wrong! cannot Get User Data","error":err.message})
     }
 
 
@@ -42,9 +43,9 @@ app.post("/adduser", async (req, res) => {
     try {
         const newData = new UserModel(data)
         await newData.save()
-        res.send(newData)
+        res.send({"msg":"User added successfully"})
   } catch (err) {
-        res.send(err)
+    res.send({"msg":"somthing went wrong! cannot add new user ","error":err.message})
     }
 
 
@@ -57,9 +58,9 @@ app.put("/users/update/:id", async (req, res) => {
     console.log(data,id)
     try { 
 let newData=await UserModel.findOneAndUpdate({"_id":id},data)
-res.send(newData)
+res.send({"msg":"User data has been updated"})
     } catch (err) {
-res.send(err)
+        res.send({"msg":"somthing went wrong! cannot update user data ","error":err.message})
      }
 
 
@@ -70,9 +71,9 @@ app.delete("/users/delete/:id", async (req, res) => {
     const id=req.params.id
     try { 
     let data=await UserModel.findOneAndDelete({"_id":id})
-    res.send(data)
+    res.send({"msg":"User has been deleted"})
     } catch (err) {
-res.send(err)
+        res.send({"msg":"somthing went wrong! cannot delete user ","error":err.message})
      }
 
 })
