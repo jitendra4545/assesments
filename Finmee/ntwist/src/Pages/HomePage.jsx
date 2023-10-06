8957d7438e95789ec3b204e09f88b5e560cf7114
jitendra4545 <jitendra45.js45@gmail.com>
2023-10-06 12:12:52 +0530

    added navbar

---
+++ Finmee/ntwist/src/Pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Navbar } from '../components/Navbar'
+import { Box } from '@chakra-ui/react'
+
+export const HomePage = () => {
+  return (
+    <Box>
+<Box h='100vh' w='100vw' 
+backgroundImage={"https://ntwist.com/wp-content/uploads/2023/01/home-image-bg-v6.png"} 
+>
+        <Navbar/>
+        </Box>
+    </Box>
+        
+        
+    
+  )
+}
---
+++ Finmee/ntwist/src/components/Navbar.jsx
@@ -0,0 +1,71 @@
+import { Box, Image, Link, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Tab, TabIndicator, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import { AiFillCaretDown } from 'react-icons/ai'
+const inds_opt = [
+  "Sustainability", "Mineral Processing", "Mine-to-mill-to-mine optimization", "Oil & Gas"
+]
+
+
+export const Navbar = () => {
+  const [isscroll, setisscroll] = useState(false);
+
+
+
+
+  function isScrolling() {
+    if (window.scrollY > 80) {
+      setisscroll(true);
+    } else {
+      setisscroll(false);
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", isScrolling);
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <Box bg={isscroll ? "blue" : ""} zIndex='1000' position='sticky' top='0px' p='5px 50px' border={'2px solid red'} display={'flex'} justifyContent={'space-between'}>
+      <Box w='180px' border={'2px solid red'} >
+        <Image w='100%' src='https://ntwist.com/wp-content/uploads/2021/12/ntwistlight.png' />
+      </Box>
+      <Box border={'2px solid red'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+        <Box gap='7' fontSize={'lg'} color={isscroll ? "blue" : "white"} display={'flex'}  >
+          <Link><Text _hover={{ color: "white" }}>Home</Text></Link>
+
+          <Popover trigger="hover" >
+            <PopoverTrigger>
+              <Box display={'flex'}>
+                <Link><Text _hover={{ color: "white" }} >Industries </Text></Link>
+                <Text mt='5px'><AiFillCaretDown /></Text>
+              </Box>
+
+
+            </PopoverTrigger>
+            <PopoverContent>
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <PopoverHeader></PopoverHeader>
+              <PopoverBody>
+
+                {
+                  inds_opt.map((el) => {
+                    return <Text color={'black'}>{el}</Text>
+                  })
+                }
+
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
+         
+
+          <Link><Text _hover={{ color: "white" }}>AI Software</Text></Link>
+          <Link><Text _hover={{ color: "white" }}>Blog</Text></Link>
+          <Link><Text _hover={{ color: "white" }}>Contact Us</Text></Link>
+
+        </Box>
+      </Box>
+    </Box>
+  )
+}
